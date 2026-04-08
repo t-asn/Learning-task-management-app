ecc33b88e89d1c44f4a2b93045d9aedab1b2e5d3
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-04-08 11:49:18 +0900

    TaskRepositoryImpl.javaへのリポジトリ実装

--- src/main/java/com/example/dao/TaskDao.java
+++ src/main/java/com/example/dao/TaskDao.java
@@ -3,8 +3,20 @@ package com.example.dao;
 import com.example.model.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
+/**
+ * Spring Data JDBC を利用したデータアクセス用インターフェース。
+ */
 @Repository
 public interface TaskDao extends CrudRepository<Task, Integer> {
+
+  /**
+   * タスクをIDの降順で全件取得します。
+   * 派生クエリメソッドを使用することで、戻り値を直接 List 型として定義でき、呼び出し元での Iterable からの強制キャストを不要にします。
+   *
+   * @return ID降順にソートされたタスクのリスト
+   */
+  List<Task> findAllByOrderByIdDesc();
 
 }
