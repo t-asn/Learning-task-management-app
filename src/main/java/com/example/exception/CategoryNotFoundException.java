78d00cc26e48011360ecf9c17e8ed730b905fb2d
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-04-10 12:37:53 +0900

    CategoryNotFoundExceptionの追加

---
+++ src/main/java/com/example/exception/CategoryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.exception;
+
+/**
+ * 指定されたカテゴリがデータベースに存在しない場合にスローされる例外。
+ */
+public class CategoryNotFoundException extends RuntimeException {
+  public CategoryNotFoundException(String message) {
+    super(message);
+  }
+}
