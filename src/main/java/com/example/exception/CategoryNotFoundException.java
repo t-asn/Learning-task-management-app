d3648455010437fd8b79dc4a394d120a40e26f1c
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-04-10 12:39:55 +0900

    exceptionJavaDocの修正

--- src/main/java/com/example/exception/CategoryNotFoundException.java
+++ src/main/java/com/example/exception/CategoryNotFoundException.java
@@ -4,6 +4,7 @@ package com.example.exception;
  * 指定されたカテゴリがデータベースに存在しない場合にスローされる例外。
  */
 public class CategoryNotFoundException extends RuntimeException {
+
   public CategoryNotFoundException(String message) {
     super(message);
   }
--- src/main/java/com/example/exception/InvalidPageException.java
+++ src/main/java/com/example/exception/InvalidPageException.java
@@ -1,15 +1,10 @@
 package com.example.exception;
 
 /**
- * ページネーションにおいて、範囲外のページ番号や不正な値が指定された場合にスローされる例外。
+ * ページネーションにおいて、範囲外のページ番号が指定された場合にスローされる例外。
  */
 public class InvalidPageException extends RuntimeException {
 
-  /**
-   * 例外メッセージを受け取るコンストラクタ。
-   *
-   * @param message エラー詳細メッセージ
-   */
   public InvalidPageException(String message) {
     super(message);
   }
