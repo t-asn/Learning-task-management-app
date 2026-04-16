1824f507b04282ff9c6b38a5acf35eec04d7eb7b
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-04-16 13:53:41 +0900

    sql文の旧箇所削除

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,15 @@
-DROP TABLE IF EXISTS tasks;
-DROP TABLE IF EXISTS categories;
-
-CREATE TABLE categories
+CREATE TABLE IF NOT EXISTS categories
 (
     id   SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL UNIQUE
+    name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE tasks
-(
-    id          SERIAL PRIMARY KEY,
-    title       VARCHAR(50) NOT NULL,
-    category_id INTEGER     NOT NULL REFERENCES categories (id),
-    due_date    DATE        NOT NULL
-);
 CREATE TABLE IF NOT EXISTS tasks
 (
     id          SERIAL PRIMARY KEY,
     title       VARCHAR(50) NOT NULL,
-    category_id INT         NOT NULL,
+    category_id INTEGER     NOT NULL REFERENCES categories (id),
     due_date    DATE        NOT NULL,
     status      VARCHAR(10) NOT NULL DEFAULT 'TODO',
-    FOREIGN KEY (category_id) REFERENCES categories (id),
     CONSTRAINT chk_status CHECK (status IN ('TODO', 'DOING', 'DONE'))
 );
